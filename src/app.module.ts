2e2a75c3fafd40d9ab2bd193e1d038b124da19ac
marciohubees <marcio@netpark.com.br>
2026-06-02 14:54:32 -0300

    feat: configure cors, rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,19 @@ import { LoggingMiddleware } from './middleware/logging/logging.middleware';
     }),
     ThrottlerModule.forRoot([
       {
-        ttl: 6000,
-        limit: 100,
+        name: 'short',
+        ttl: 1000, // 1 segundo
+        limit: 10, // 10 requisições por segundo
+      },
+      {
+        name: 'medium',
+        ttl: 60000, // 1 minuto
+        limit: 100, // 100 requisições por minuto
+      },
+      {
+        name: 'long',
+        ttl: 900000, // 15 minutos
+        limit: 1000, // 1000 requisições por 15 minutos
       },
     ]),
     ProxyModule,
